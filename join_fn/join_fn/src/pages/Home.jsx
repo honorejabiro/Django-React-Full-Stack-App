731888e136bb35150e951f1b7357ef50249997cf
Honore Jabiro <honorejabiro@gmail.com>
2024-11-08 22:10:26 -0600

    Created a Notes.jsx component

--- join_fn/join_fn/src/pages/Home.jsx
+++ join_fn/join_fn/src/pages/Home.jsx
@@ -1,59 +1,95 @@
-import React from 'react'
-import {useState, useEffect} from "react";
-import api from "../api.js"
+import React from "react";
+import { useState, useEffect } from "react";
+import api from "../api.js";
 const Home = () => {
-  const [notes, setNotes] = useState([])
-  useEffect( () => {
-    const fetchnotes = () => {
-      api.get("api/notes/")
-    .then(response => {
-      console.log(response)
-      setNotes(response.data)
-    })
-    .catch(errors => {
-      console.log(errors.message)
-    })
-    }
-    fetchnotes()
-  }
-  ,[])
-  const delete_note = (id) => {
-    api.delete(`api/note/delete/${id}`)
-    .then(response => {
-      if (response.status === 204){
-        alert("Note succefully deleted")
-      }
-      else{
-        alert("Note not deleted")
-      }
-      console.log(response.message)
-    })
-    .catch(errors => {
-      console.log(errors.message)
-    })
-    fetchnotes()
-  }
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("")
+  const fetchnotes = () => {
+    api
+      .get("/api/notes/")
+      .then((response) => {
+        console.log(response);
+        setNotes(response.data);
+      })
+      .catch((errors) => {
+        console.log(errors.message);
+      });
+  };
 
-  const create_note = (title, body, category) => {
-    api.post("api/notes/", {title, body, category})
-    .then(response => {
-      if(response === 200){
-        alert("Note successfully created")
-      }
-      else{
-        alert("Note not created")
-      }
-    })
-    .catch(error => {
-      console.log(error.message)
-    })
+  useEffect(() => {
     fetchnotes()
-  }
+  }, [])
+
+
+  const delete_note = (id) => {
+    api
+      .delete(`/api/note/delete/${id}`)
+      .then((response) => {
+        if (response.status === 204) {
+          alert("Note succefully deleted");
+          fetchnotes();
+        } else {
+          alert("Note not deleted");
+        }
+        console.log(response.message);
+      })
+      .catch((errors) => {
+        console.log(errors.message);
+      });
+  };
+
+  const create_note = (e) => {
+    e.preventDefault()
+    api
+      .post("/api/notes/", { title, body })
+      .then((response) => {
+        if (response.status === 201) {
+          alert("Note successfully created");
+          fetchnotes();
+        } else {
+          alert("Failed to make note");
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
     <div>
-      Homre
+      <div>
+        <h1>
+          Notes
+        </h1>
+      </div>
+      <h2></h2>
+      <form onSubmit={create_note}>
+        <label htmlFor="title"> Title </label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={(e) => {
+            setTitle(e.target.value)
+          }}
+          value={title}
+        />
+        <label htmlFor="conent"> Content: </label>
+        <br />
+        <textarea
+          id="content"
+          name="content"
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        ></textarea>
+        <br />
+        <input type="submit" value="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
